080e48d9084cac17e42ee71cfa75b7177353f807
Cheng Lou <chenglou92@gmail.com>
2017-09-06 18:10:46 -0700

    Final example revamp

--- src/interop/myBanner.js
+++ src/interop/myBanner.js
@@ -1,3 +1,6 @@
+// This file isn't used directly by JS; it's used to myBanner.re, which is then
+// used by the ReasonReact component GreetingRe.
+
 var ReactDOM = require('react-dom');
 var React = require('react');
 
@@ -6,7 +9,7 @@ var App = React.createClass({
   render: function() {
     if (this.props.show) {
       return React.createElement('div', null,
-        this.props.message
+        'Here\'s the message from the owner: ' + this.props.message
       );
     } else {
       return null;
