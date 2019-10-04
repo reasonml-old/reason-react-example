a505d14c4f69e0ecbb33dc0ef397dec3a21d2f13
Cheng Lou <chenglou92@gmail.com>
2019-10-03 21:42:22 -0700

    Comment the watcher

--- watcher.js
+++ watcher.js
@@ -1,26 +1,28 @@
-
+// This is our simple, robust watcher. It hooks into the BuckleScript build
+// system to listen for build events.
+// See package.json's `start` script and `./node_modules/.bin/bsb --help`
 
 var wsReloader;
-var LAST_SUCCESS_BUILD_STAMP = (localStorage.getItem('LAST_SUCCESS_BUILD_STAMP') || 0)
+var LAST_SUCCESS_BUILD_STAMP = localStorage.getItem('LAST_SUCCESS_BUILD_STAMP' || 0)
 var WS_PORT = 9999; // configurable
 
 function setUpWebScoket() {
-    if (wsReloader == null || wsReloader.readyState !== 1) {
-        try {
-            wsReloader = new WebSocket(`ws://localhost:${WS_PORT}`)
-            wsReloader.onmessage = (msg) => {
-                var newData = JSON.parse(msg.data).LAST_SUCCESS_BUILD_STAMP
-                if (newData > LAST_SUCCESS_BUILD_STAMP) {
-                    LAST_SUCCESS_BUILD_STAMP = newData
-                    localStorage.setItem('LAST_SUCCESS_BUILD_STAMP', LAST_SUCCESS_BUILD_STAMP)
-                    location.reload(true)
-                }
-
-            }
-        } catch (exn) {
-            console.error("web socket failed connect")
+  if (wsReloader == null || wsReloader.readyState !== 1) {
+    try {
+      wsReloader = new WebSocket(`ws://localhost:${WS_PORT}`)
+      wsReloader.onmessage = (msg) => {
+        var newData = JSON.parse(msg.data).LAST_SUCCESS_BUILD_STAMP
+        if (newData > LAST_SUCCESS_BUILD_STAMP) {
+          LAST_SUCCESS_BUILD_STAMP = newData
+          localStorage.setItem('LAST_SUCCESS_BUILD_STAMP', LAST_SUCCESS_BUILD_STAMP)
+          location.reload(true)
         }
+
+      }
+    } catch (exn) {
+      console.error("web socket failed connect")
     }
+  }
 };
 
 setUpWebScoket();
