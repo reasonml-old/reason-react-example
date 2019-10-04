0db5db865df90dc4f5bb00ee56de236676e329f9
Cheng Lou <chenglou92@gmail.com>
2019-10-03 17:43:32 -0700

    Tweak

--- src/InteropExample/ReactJSButtonWrapper.js
+++ src/InteropExample/ReactJSButtonWrapper.js
@@ -4,12 +4,15 @@ var React = require('react');
 var ReasonReactButton = require('./ReasonReactButton.bs').make;
 
 var ReactJSComponent = function() {
-  // We're not using JSX here, to avoid folks needing to install the related
-  // React toolchains just for this example.
-
   let backgroundColor = "rgba(0, 0, 0, 0.05)";
   let padding = "12px";
 
+  // We're not using JSX here, to avoid folks needing to install the related
+  // React toolchains just for this example.
+  // <div style={...}>
+  //   <div style={...}>This is a ReactJS card</div>
+  //   <ReasonReactButton style={...} />
+  // </div>
   return React.createElement(
     "div",
     {style: {backgroundColor, padding, borderRadius: "8px"}},
